8bb47fbdad8322ccd5332645318d5898067186fd
Valentín <61363314+Valo21@users.noreply.github.com>
2026-02-19 14:51:42 -0300

    chore(styles): :fire: update styles

--- enviar-mail.php
+++ enviar-mail.php
@@ -1,18 +1,17 @@
 <?php
-  if ($_POST["nombre"] && $_POST["email"] && $_POST["mensaje"]) {
+  if (!empty($_POST["nombre"]) && !empty($_POST["email"]) && !empty($_POST["mensaje"])) {
     $nombre = $_POST['nombre'];
     $email = $_POST['email'];
     $mensaje = $_POST['mensaje'];
 
     $to = "tucorreo@dominio.com";
     $subject = "Nueva solicitud de servicio";
-    $body = "Nombre: $nombre\nEmail: $email\nMensaje:\n$mensaje";
-
-    mail($to, $subject, $body);
-
-    header("refresh:3;url=../index.php");
+    $body = "Nombre:".$nombre."\nEmail:".$email."\nMensaje:\n".$mensaje;
+    
+    echo '<p>Enviando mail a '.$email."</p>";
+    header("refresh:0;url=index.php");
   } else {
     echo "<p>Error, redireccionando...</p>";
-    header("refresh:3;url=../index.php");
+    header("refresh:3;url=index.php");
   }
 ?>
